2be86a0d5dfca7ca9ce3b3415a3c4e1949abeed7
Andrew J Dyck <web@andrewdyck.com>
2016-02-13 13:49:21 -0600

    random forest

--- modelFit.R
+++ modelFit.R
@@ -1,16 +1,18 @@
 
 library(caret)
 train_in <- read.csv('./pml-training.csv', header=T)
-test_in <- read.csv('./pml-testing.csv', header=T, colClasses = sapply(training, class))
+validation <- read.csv('./pml-testing.csv', header=T)
 #training$classe <- as.factor(training$classe)
 #testing$classe <- as.factor(testing$classe)
 
-training
 set.seed(127)
+training_sample <- createDataPartition(y=train_in$classe, p=0.7, list=FALSE)
+training <- train_in[training_sample, ]
+testing <- train_in[-training_sample, ]
 #folds <- createFolds(y=training$classe, k=3, list=TRUE, returnTrain=TRUE)
 
-preObj <- preProcess(training[, 1:(nrow(training)-1)], method =c('center', 'scale'))
-featurePlot(x=training[, 15:17], y=training$classe, plot = 'pairs')
+#preObj <- preProcess(training[, 1:(nrow(training)-1)], method =c('center', 'scale'))
+#featurePlot(x=training[, 15:17], y=training$classe, plot = 'pairs')
 
 # PCA
 #prComp <- prcomp(var1, var2)
@@ -18,25 +20,26 @@ featurePlot(x=training[, 15:17], y=training$classe, plot = 'pairs')
 #testPC <- predict(preProc, testing[, 1:2]) # predicts preprocessed training data
 
 #feature selection
-set.seed(10)
-ctrl <- rfeControl(functions = lmFuncs,
-                   method = "repeatedcv",
-                   repeats = 5,
-                   verbose = FALSE)
-lmProfile <- rfe(x, y,
-                 sizes = subsets,
-                 rfeControl = ctrl)
-lmProfile
+#set.seed(127)
+# ctrl <- rfeControl(functions = lmFuncs,
+#                    method = "repeatedcv",
+#                    repeats = 5,
+#                    verbose = FALSE)
+# lmProfile <- rfe(x, y,
+#                  sizes = subsets,
+#                  rfeControl = ctrl)
+# lmProfile
 
 fitControl <- trainControl(method='cv', number = 3)
 model <- train(
-  classe ~ ., data=training[, 8:ncol(training)],
+  classe ~ ., data=training,
   trControl=fitControl,
   #preProcess=c('center', 'scale'),
-  method='rpart'
+  method='rf'
 )
-plot(model$finalModel)
-text(model$finalModel)
+save(model, './ModelFitRandomForest.RData')
+# plot(model$finalModel)
+# text(model$finalModel)
 
-pred <- predict(model, newdata=testing[, 8:ncol(testing)])
-confusionMatrix(pred, testing$classe)
+pred <- predict(model, newdata=testing)
+confusionMatrix(pred, na.omit(testing)$classe)
