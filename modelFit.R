781c0251b023f20d6894cfd98a0978cb8e254f40
Andrew Dyck <info@andrewdyck.com>
2016-02-13 13:02:50 -0600

    model update

--- modelFit.R
+++ modelFit.R
@@ -1,15 +1,16 @@
 
 library(caret)
-training <- read.csv('./pml-training.csv', header=T)
-testing <- read.csv('./pml-testing.csv', header=T)
+train_in <- read.csv('./pml-training.csv', header=T)
+test_in <- read.csv('./pml-testing.csv', header=T, colClasses = sapply(training, class))
+#training$classe <- as.factor(training$classe)
+#testing$classe <- as.factor(testing$classe)
 
+training
 set.seed(127)
 #folds <- createFolds(y=training$classe, k=3, list=TRUE, returnTrain=TRUE)
 
 preObj <- preProcess(training[, 1:(nrow(training)-1)], method =c('center', 'scale'))
-featurePlot(x=training[, 1:10], y=training$classe, plot = 'pairs')
-
-fitControl <- trainControl(method='cv', number = 3)
+featurePlot(x=training[, 15:17], y=training$classe, plot = 'pairs')
 
 # PCA
 #prComp <- prcomp(var1, var2)
@@ -27,12 +28,15 @@ lmProfile <- rfe(x, y,
                  rfeControl = ctrl)
 lmProfile
 
-
+fitControl <- trainControl(method='cv', number = 3)
 model <- train(
-  classe ~ var_accel_forearm, data=training,
+  classe ~ ., data=training[, 8:ncol(training)],
   trControl=fitControl,
-  preProcess=c('center', 'scale'),
+  #preProcess=c('center', 'scale'),
   method='rpart'
 )
+plot(model$finalModel)
+text(model$finalModel)
 
-
+pred <- predict(model, newdata=testing[, 8:ncol(testing)])
+confusionMatrix(pred, testing$classe)
